2a3f3cceca54c953677e8c3ec63a5af0e0d223c1
wiro34 <too@wirog.net>
2012-12-22 08:00:48 +0900

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,30 @@
+#!/bin/sh
 
-ln -s ~/.dotfiles/.bashrc   ~/.bashrc
-ln -s ~/.dotfiles/.zshrc    ~/.zshrc
-ln -s ~/.dotfiles/.zshenv   ~/.zshenv
-ln -s ~/.dotfiles/.vimrc    ~/.vimrc
+FILES=(
+  .bashrc
+  .zshrc
+  .zshenv
+  .vimrc
+  .gitconfig
+)
 
+DIR=$HOME/.dotfiles
+
+for FILE in ${FILES[@]} ; do
+  if [ -e $HOME/$FILE ] ; then
+    if [ ! -ef $HOME/$FILE.bak  ] ; then
+      mv $HOME/$FILE $HOME/$FILE.bak
+    else
+      rm $HOME/$FILE
+    fi
+  fi
+  ln -s $DIR/$FILE $HOME/$FILE
+done
+
+#if [ -e ~/.bashrc ] ; then
+#
+#ln -s ~/.dotfiles/.bashrc    ~/.bashrc
+#ln -s ~/.dotfiles/.zshrc     ~/.zshrc
+#ln -s ~/.dotfiles/.zshenv    ~/.zshenv
+#ln -s ~/.dotfiles/.vimrc     ~/.vimrc
+#ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
